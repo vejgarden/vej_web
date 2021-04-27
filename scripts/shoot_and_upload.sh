361059c7b1ae2f8775a57b72f07603369caa6bf0
Sung Eun Bae <sungeunbae@live.com>
2021-04-27 23:57:06 +1200

    No photo upload during night

--- scripts/shoot_and_upload.sh
+++ scripts/shoot_and_upload.sh
@@ -4,7 +4,12 @@ img_file=`cat /tmp/shoot.log`
 weather_file=/home/pi/weather_data/`basename ${img_file/.jpg/.json}`
 python /home/pi/vej_web/scripts/get_weather.py $weather_file
 
-scp $img_file vej_ec2:/home/ubuntu/vej_web/static/photo
+hour=`date +"%H"`
+if [ $hour -le 17 ] && [ $hour -ge 07 ]
+then
+	scp $img_file vej_ec2:/home/ubuntu/vej_web/static/photo
+fi
+
 scp $weather_file vej_ec2:/home/ubuntu/vej_web/static/weather
 ssh -t vej_ec2 "cd /home/ubuntu/vej_web && /home/ubuntu/py385/bin/python sqlalchemy_weather_update.py"
 
