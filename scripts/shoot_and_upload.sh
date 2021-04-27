ab70cf93b2b0da260608e2caa2c69b5ed79467db
Sung Eun Bae <sungeunbae@live.com>
2021-04-27 21:04:28 +1200

    Fixed path

--- scripts/shoot_and_upload.sh
+++ scripts/shoot_and_upload.sh
@@ -2,7 +2,7 @@
 /home/pi/bin/shoot >/tmp/shoot.log
 img_file=`cat /tmp/shoot.log`
 weather_file=/home/pi/weather_data/`basename ${img_file/.jpg/.json}`
-python get_weather.py $weather_file
+python /home/pi/bin/get_weather.py $weather_file
 
 scp $img_file vej_ec2:/home/ubuntu/vej_web/static/img
 scp $weather_file vej_ec2:/home/ubuntu/vej_web/static/weather
