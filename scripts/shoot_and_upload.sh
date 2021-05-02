54b01f06ca59042edb6e78271b3811d947ab1a4c
Sung Eun Bae <sungeunbae@live.com>
2021-05-02 20:54:51 +1200

    Update data every 10 minutes

--- scripts/shoot_and_upload.sh
+++ scripts/shoot_and_upload.sh
@@ -5,10 +5,11 @@ weather_file=/home/pi/weather_data/`basename ${img_file/.jpg/.json}`
 python /home/pi/vej_web/scripts/get_weather.py $weather_file
 
 hour=`date +"%H"`
-if [ $hour -le 17 ] && [ $hour -ge 07 ]
-then
-	scp $img_file vej_ec2:/home/ubuntu/vej_web/static/photo
-fi
+#if [ $hour -le 17 ] && [ $hour -ge 07 ]
+#then
+#	scp $img_file vej_ec2:/home/ubuntu/vej_web/static/photo
+#fi
+scp $img_file vej_ec2:/home/ubuntu/vej_web/static/photo
 
 scp $weather_file vej_ec2:/home/ubuntu/vej_web/static/weather
 ssh -t vej_ec2 "cd /home/ubuntu/vej_web && /home/ubuntu/py385/bin/python sqlalchemy_weather_update.py"
